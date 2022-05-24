eca74820109bb76af41aa4b70ca98f5a73631c21
ryomaemura <ryomaemura34@gmail.com>
2022-05-24 14:31:12 +0900

    add score-label

--- Assets/Scenes/NumberManager.cs
+++ Assets/Scenes/NumberManager.cs
@@ -16,6 +16,7 @@ public class NumberManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI buttonText8;
     [SerializeField] TextMeshProUGUI buttonText9;
     [SerializeField] TextMeshProUGUI buttonText10;
+    [SerializeField] TextMeshProUGUI scoreLabel;
     TextMeshProUGUI[] buttonTexts;
     int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     int[, ] relasionshipNumbers = {
@@ -26,7 +27,7 @@ public class NumberManager : MonoBehaviour
         {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
         {0, 0, 1, 0, 0, 1, 0, 1, 0, 0},
 
-        {0, 0, 0, 1, 1, 0, 1, 0, 0, 1},
+        {0, 0, 0, 1, 1, 0, 1, 0, 0, 0},
         {0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
         {0, 1, 0, 0, 1, 0, 0, 0, 0, 1},
         {0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
@@ -35,6 +36,7 @@ public class NumberManager : MonoBehaviour
     int temp = 0;
     int randomNumber1 = 0;
     int randomNumber2 = 0;
+    int scoreNumber = 0;
 
     // Start is called before the first frame update
     void Start() {
@@ -42,6 +44,7 @@ public class NumberManager : MonoBehaviour
 
         shuffleNumbers();
         setNumbers();
+        scoreReload();
     }
 
     // Update is called once per frame
@@ -57,6 +60,9 @@ public class NumberManager : MonoBehaviour
 
                 buttonTexts[number - 1].text = "";
                 buttonTexts[i].text = numbers[i].ToString();
+
+                scoreNumber = scoreNumber + 1;
+                scoreReload();
             }
         }
     }
@@ -80,5 +86,12 @@ public class NumberManager : MonoBehaviour
             numbers[randomNumber1] = numbers[randomNumber2];
             numbers[randomNumber2] = temp;
         }
+
+        scoreNumber = 0;
+        scoreReload();
+    }
+
+    public void scoreReload() {
+        scoreLabel.text = "Move:" + scoreNumber.ToString();
     }
 }
